2583aa93b83103594916eaacaebb3fec0b2ec237
Иван Едоков <ivanedokov@yandex.ru>
2024-01-24 18:09:47 +0400

    ревью 1

--- app/crud/charity_project.py
+++ app/crud/charity_project.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import List, Optional
 
 from sqlalchemy import select
 
@@ -25,6 +25,23 @@ class CRUDCharityProject(CRUDBase):
         )
         db_project_id = db_project_id.scalars().first()
         return db_project_id
+
+    async def get_projects_by_completion_rate(
+            self,
+            session: AsyncSession,
+    ) -> List[CharityProject]:
+        """
+        Функция отсортирует список со всеми закрытыми проектами
+        по количеству времени, которое понадобилось на сбор средств,
+        — от меньшего к большему.
+        """
+        charity_projects = await session.execute(
+            select(CharityProject).where(
+                CharityProject.fully_invested
+            ).order_by(CharityProject.close_date - CharityProject.create_date)
+        )
+        charity_projects = charity_projects.scalars().all()
+        return charity_projects
 
 
 charity_project_crud = CRUDCharityProject(CharityProject)
